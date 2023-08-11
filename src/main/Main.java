2b70fc2a4dabbbccca91f9a81de11722bc3b5429
hacim17h <hacim17h@gmail.com>
2023-08-10 23:04:24 -0400

    Add additional language tests

--- src/main/Main.java
+++ src/main/Main.java
@@ -43,9 +43,18 @@ public class Main extends Application {
 
         //Locale.setDefault(new Locale("fr","FR"));
         ResourceBundle rb = ResourceBundle.getBundle("helper/Lang", Locale.getDefault());
+        //language tests
         System.out.println("The username is: " + rb.getString("Username"));
         System.out.println("The password is: " + rb.getString("Password"));
+        System.out.println("The Login is: " + rb.getString("Login"));
+        System.out.println("The Exit is: " + rb.getString("Exit"));
+        System.out.println("The Application Manager is: " + rb.getString("Application Manager"));
+        System.out.println("The Application Manager Login is: " + rb.getString("Application Manager Login"));
+        System.out.println("The Locale is: " + rb.getString("Locale"));
+        System.out.println("The error message is: " + rb.getString("The username and/or password do not match."));
+        //default locale test
         System.out.println("The default locale is: " + Locale.getDefault());
+
         launch(args);
         JDBC.closeConnection();
     }
