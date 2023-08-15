a6698c94f9e4628d438ba90895f98a283069b4fb
hacim17h <hacim17h@gmail.com>
2023-08-15 10:13:13 -0400

    Add basic navigation for add customer form

--- src/controller/AddCustomerController.java
+++ src/controller/AddCustomerController.java
@@ -1,12 +1,28 @@
 package controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AddCustomerController {
+    /**
+     * Stores the stage.
+     */
+    Stage stage;
+
+    /**
+     * Stores the scene.
+     */
+    Parent scene;
 
     @FXML
     private TextField addCustomerAddressTxt;
@@ -37,5 +53,33 @@ public class AddCustomerController {
 
     @FXML
     private Button addCustomerSaveBtn;
+
+    /**
+     * Returns to the view customer form. When the button, is pressed the returns to the previous view customer form.
+     * @param event helps get the window that caused the event
+     * @throws IOException for handling any input output exception
+     */
+    @FXML
+    void onActionCancel(ActionEvent event) throws IOException {
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/ViewCustomerForm.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
+
+    /**
+     * Returns to the view customer form after saving input. When the button is pressed, the form returns to the
+     * previous view customer form and saves the updated input data to the database as well as displaying it to the
+     * tableview.
+     * @param event helps get the window that caused the event
+     * @throws IOException for handling any input output exception
+     */
+    @FXML
+    void onActionSave(ActionEvent event) throws IOException {
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/ViewCustomerForm.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
+    }
 
 }
